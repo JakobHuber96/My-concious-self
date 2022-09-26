ca2523faaab053f90e56b6440d6d00e2080bca9c
Jakob Huber <93046@psdschools.org>
2022-09-26 11:16:04 -0600

    Initial Commit

---
+++ My concious self/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My concious self
+//
+//  Created by Huber, Jakob - Student on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
